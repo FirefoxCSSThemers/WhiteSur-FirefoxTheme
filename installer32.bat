514469cb90d0a44370e0fa17b81a6b50ba80192f
Javier Latorre <65726176+TheGamerPro0013@users.noreply.github.com>
2021-05-24 14:15:28 +0200

    Updated the script.

--- installer32.bat
+++ installer32.bat
@@ -1,5 +1,5 @@
 @echo off
-echo This script works for both 32-Bit and 64-Bit CPUs. If you want to download the 64-Bit version, go here: (uri)
+echo This script works for both 32-Bit and 64-Bit CPUs. If you want to download the 64-Bit version, go here: https://github.com/FirefoxCSSThemers/WhiteSur-Tweaked/raw/main/installer64.bat
 timeout 5
 echo Searching for already installed packages...
 cd C:\
